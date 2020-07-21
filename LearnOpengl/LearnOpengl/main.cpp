1126f4003ec743d621af77accfe81181f144d5d6
zhaowanfei <zhaowanfei@h3d.com.cn>
2020-07-21 16:31:11 +0800

    add main.cpp

--- LearnOpengl/LearnOpengl/main.cpp
+++ LearnOpengl/LearnOpengl/main.cpp
@@ -1,9 +1,48 @@
 #pragma once
-#include "Lighting/BasicLighting.h"
-#include "PBR/PBR_Lighting.h"
+//#include "GetStarted/Triangle.h"
+//#include "GetStarted/Triangle_Practice.h"
+//#include "GetStarted/Shader_Practice.h"
+//#include "GetStarted/Textures.h"
+
+//#include "GetStarted/HelloWindow.h"
+//#include "GetStarted/HelloWindow_Practice.h"
+//#include "Lighting/BasicLighting.h"
+//#include "Lighting/4.2Materials.h"
+//#include "Lighting/4.3LightingMaps.h"
+
+
+#include "AdvancedOpengl/GeometryShader.h"
+
+//#include "AdvancedLighting/SSAO.h"
+//#include "AdvancedLighting/StencilTesting.h"
+//#include "AdvancedLighting/ShadowMap.h"
+//#include "AdvancedLighting/DepthMap.h"
+//#include "AdvancedLighting/Hdr.h"
+
+//#include "PBR/PBR_Lighting.h"
+
 
 int main()
 {
-	PBR::pbr_main();
+	//HelloWindow_Practice::main();
+	//Triangle::main();
+	//Triangle_Practice::main();
+	//Textures::main();
+
+	//ShaderPractice::main();
+
+	//BasicLighting::main();
+	//Materials::main();
+	//LightingMaps::main();
+
+	GeometryShader::main();
+
+	//ssao::ssao_main();
+	//StencilTesting::main();
+	//ShadowMap::main();
+	//DepthMap::main();
+	//HDR::main();
+
+	//PBR::pbr_main();
 	return 0;
 }
